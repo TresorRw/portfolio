87ff8eae2374f4ae845c190391f548c8d785c3e5
TresorRw <alaintresorcyusa683@gmail.com>
2023-12-12 01:29:33 +0200

    form validation

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,18 +1,52 @@
 'use client'
 import { Button, Divider, Input, Textarea } from '@nextui-org/react';
+import { useForm } from 'react-hook-form';
 import { AiOutlineSend } from 'react-icons/ai';
 
 const classnames = {
   input: 'text-success font-semibold',
   label: 'text-primary-500 font-semibold'
 }
+interface formData {
+  names: string,
+  email: string,
+  message: string
+}
 
 const ContactForm = () => {
+  const { register, formState: { errors }, reset, handleSubmit } = useForm<formData>();
+  const submitMessage = (data: formData) => {
+    console.log(data);
+    reset();
+  }
+
   return (
-    <form className='space-y-4 mt-5 text-center'>
+    <form onSubmit={handleSubmit(submitMessage)} className='mt-5 space-y-2'>
       <Divider />
-      <Input type='email' name='email' classNames={classnames} color='primary' required label={'Your email'}/>
-      <Textarea label="What is on your mind?" name='message' color='primary' required classNames={classnames} />
+      <Input
+        type='text' {...register('names', { required: true })}
+        classNames={classnames} color='primary'
+        required
+        label={'Your Names'}
+      />
+      {errors.names && <p className="text-danger">Your names are required</p>}
+      <Input
+        type='email'
+        {...register('email', {
+          required: 'A valid email is required',
+          pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: 'Please provide a valid email address'
+          }
+        })}
+        classNames={classnames}
+        color='primary'
+        required
+        label={'Your email'}
+      />
+      {errors.email && <p>{errors.email.message}</p>}
+      <Textarea label="What is on your mind?" {...register('message', { required: { value: true, message: 'Provide your message please' } })} color='primary' required classNames={classnames} />
+      {errors.message && <p>{errors.message.message}</p>}
       <Button type='submit' size="lg" startContent={<AiOutlineSend />} variant="ghost" color='success'>Send my message</Button>
     </form>
   )
