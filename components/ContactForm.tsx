3e8cf0cd1ec5ef8066174447464c94dcd611bf58
TresorRw <alaintresorcyusa683@gmail.com>
2023-12-12 00:17:39 +0200

    Contact Form component

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -9,11 +9,11 @@ const classnames = {
 
 const ContactForm = () => {
   return (
-    <form className='space-y-4 mt-5'>
+    <form className='space-y-4 mt-5 text-center'>
       <Divider />
       <Input type='email' name='email' classNames={classnames} color='primary' required label={'Your email'}/>
-      <Textarea label="What's on your mind?" name='message' color='primary' required classNames={classnames} />
-      <Button type='submit' startContent={<AiOutlineSend />} variant="ghost" color='success'>Send my message</Button>
+      <Textarea label="What is on your mind?" name='message' color='primary' required classNames={classnames} />
+      <Button type='submit' size="lg" startContent={<AiOutlineSend />} variant="ghost" color='success'>Send my message</Button>
     </form>
   )
 }
