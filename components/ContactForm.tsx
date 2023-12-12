c0a6169a8aaa9c40842fdc84cb1b208b7375eac5
TresorRw <alaintresorcyusa683@gmail.com>
2023-12-12 04:24:14 +0200

    email sending

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,23 +1,32 @@
 'use client'
-import { Button, Divider, Input, Textarea } from '@nextui-org/react';
+import { Contact } from '@/interfaces';
+import { Button, Divider, Input, Spinner, Textarea } from '@nextui-org/react';
+import axios from 'axios';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { AiOutlineSend } from 'react-icons/ai';
 
 const classnames = {
   input: 'text-success font-semibold',
   label: 'text-primary-500 font-semibold'
 }
-interface formData {
-  names: string,
-  email: string,
-  message: string
-}
 
 const ContactForm = () => {
-  const { register, formState: { errors }, reset, handleSubmit } = useForm<formData>();
-  const submitMessage = (data: formData) => {
-    console.log(data);
-    reset();
+  const [loading, setIsLoading] = useState(false);
+  const { register, formState: { errors }, reset, handleSubmit } = useForm<Contact>();
+  const submitMessage = async (data: Contact) => {
+    setIsLoading(true);
+    toast.loading('Sending your email...');
+    const res = await axios.post('/api/send-email', data);
+    toast.remove()
+    if (res.data.success) {
+      toast.success(res.data.message)
+      reset()
+    } else {
+      toast.error(res.data.message)
+    }
+    setIsLoading(false)
   }
 
   return (
@@ -44,10 +53,16 @@ const ContactForm = () => {
         required
         label={'Your email'}
       />
-      {errors.email && <p>{errors.email.message}</p>}
-      <Textarea label="What is on your mind?" {...register('message', { required: { value: true, message: 'Provide your message please' } })} color='primary' required classNames={classnames} />
-      {errors.message && <p>{errors.message.message}</p>}
-      <Button type='submit' size="lg" startContent={<AiOutlineSend />} variant="ghost" color='success'>Send my message</Button>
+      {errors.email && <p className="text-danger">{errors.email.message}</p>}
+      <Textarea
+        label="What is on your mind?"
+        {...register('message',
+          { required: { value: true, message: 'Provide your message please' } }
+        )} color='primary' required classNames={classnames}
+      />
+      {errors.message && <p className="text-danger">{errors.message.message}</p>}
+
+      <Button type='submit' aria-disabled={loading} size="lg" startContent={<AiOutlineSend />} variant="ghost" color='success'>{loading ? <Spinner /> : 'Send my message'}</Button>
     </form>
   )
 }
