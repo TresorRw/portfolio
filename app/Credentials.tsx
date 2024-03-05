d15c7adee6bddcd1890211f7c4c29f732892d9a6
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-05 10:37:22 +0200

    Added other new certificates

--- app/Credentials.tsx
+++ app/Credentials.tsx
@@ -1,30 +1,33 @@
 import Container from "@/components/Container";
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { Image, Link } from "@nextui-org/react";
 
 export default function CredentialsSection() {
   return (
     <Container>
       <div id="certificates" className="md:px-24 md:pt-16 p-5">
         <h1 className="text-3xl font-bold mb-5 text-success">Awards &amp; Certifications</h1>
-        <div className="flex gap-4 flex-col flex-wrap md:flex-row">
-          <Card className="w-full md:max-w-[400px] lg:1/4 bg-primary-100">
-            <CardHeader>
-              <h3 className="text-center font-bold text-lg text-success">JavaScript Algorithms &amp; Data Structures</h3>
-            </CardHeader>
-            <CardBody>
-              <Image isZoomed src="/dsa.png" alt="Cyusa's JavaScript Algorithms & Data Structures certificate" />
-            </CardBody>
-          </Card>
-          <Card className="w-full md:max-w-[400px] lg:1/4 bg-primary-100">
-            <CardHeader>
-              <h3 className="text-center text-success font-bold text-lg">Back End Development &amp; APIs</h3>
-            </CardHeader>
-            <CardBody>
-              <Image isZoomed alt="Cyusa's Back End Development and APIs certificate" src="/bn-apis.png" />
-            </CardBody>
-          </Card>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div>
+            <Image isZoomed src="/ATLP_Certificate.png" alt="Andela Technical Leadership Program Certificate" className="w-full object-cover h-64" />
+            <figcaption>
+              <Link isExternal href="/ATLP_Certificate.png" showAnchorIcon className="text-white">Andela Technical Leadership Program</Link>
+            </figcaption>
+          </div>
+          <div>
+            <Image isZoomed src="/dsa.png" alt="Data Structures and Algorithms Certificate" className="w-full object-cover h-64" />
+            <figcaption>
+              <Link isExternal href="/dsa.png" showAnchorIcon className="text-white">Data Structures and Algorithms</Link>
+            </figcaption>
+          </div>
+          <div>
+            <Image isZoomed src="/bn-apis.png" alt="Back End Developemnt and APIs Certificate" className="w-full object-cover h-64" />
+            <figcaption>
+              <Link isExternal href="/bn-apis.png" showAnchorIcon className="text-white">Back End Developemnt and APIs</Link>
+            </figcaption>
+          </div>
         </div>
       </div>
-    </Container>
+    </Container >
   )
 }
